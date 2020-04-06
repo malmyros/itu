4702f5ce638e886bd5e98a91bbde4509b7b36f67
tsl-michail-almyros <michail.almyros@clarity-software.com>
2020-04-06 22:11:08 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import {Button, Container, Input, Navbar, NavbarBrand} from 'reactstrap'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      term: 'U2',
+      offset: 0,
+      limit: 25,
+    }
+  }
+
+  componentDidMount() {
+    this.onLoadData()
+  }
+
+  onLoadData = () => {
+    const {term, offset, limit} = this.state
+    fetch(`https://itunes.apple.com/search?term=${term}&offset=${offset}&limit=${limit}`, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            results: this.state.results.concat(data.results),
+            offset: this.state.offset + 25,
+            limit: this.state.limit + 25,
+          })
+        })
+  }
+
+  onChangeTerm = (e) => {
+    this.setState({
+      term: e.target.value,
+    })
+  }
+
+  onSearch = () => {
+    this.setState({
+      results: [],
+      offset: 0,
+      limit: 25,
+    }, () => this.onLoadData())
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">Itunes</NavbarBrand>
+            <Input
+                value={this.state.term || ''}
+                onChange={this.onChangeTerm}/>
+            <Button color="secondary" onClick={this.onSearch}>Search</Button>
+          </Navbar>
+          <Container>
+            <ul>
+              <InfiniteScroll
+                  dataLength={this.state.results.length}
+                  next={this.onLoadData}
+                  hasMore={true}
+                  loader={<h4>Loading...</h4>}>
+                {this.state.results
+                    .map((result, index) => {
+                      return (
+                          <li key={`result-${index}`}>
+                            <img alt={result.trackName} src={result.artworkUrl100}/>
+                            <div className="wrapper">
+                              <div className={'track-name'}>{result.trackName}</div>
+                              <div className={'artist-name'}>{result.artistName}</div>
+                              <div className={'genre'}>{result.primaryGenreName}</div>
+                              <div className={'release-date'}>{result.releaseDate}</div>
+                              <audio src={result.previewUrl} controls>
+                                Your browser does not support the audio element.
+                              </audio>
+                            </div>
+                          </li>
+                      )
+                    })
+                }
+              </InfiniteScroll>
+            </ul>
+          </Container>
+        </div>
+    );
+  }
 }
 
 export default App;
