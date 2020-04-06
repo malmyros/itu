1e7d2259e8e7b25d45f5d85d97a395097e6ca17d
tsl-michail-almyros <michail.almyros@clarity-software.com>
2020-04-06 22:23:12 +0100

    Updates

---
+++ precache-manifest.6e4459555781044bce0cb34bb241dd4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7fa3c7758e8d2b041378514eb2be04",
+    "url": "/itu/index.html"
+  },
+  {
+    "revision": "a71d7ca3de3c059ea522",
+    "url": "/itu/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "739137328cafa6418563",
+    "url": "/itu/static/css/main.a520041a.chunk.css"
+  },
+  {
+    "revision": "a71d7ca3de3c059ea522",
+    "url": "/itu/static/js/2.cf2215ef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/itu/static/js/2.cf2215ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739137328cafa6418563",
+    "url": "/itu/static/js/main.1f8aedb5.chunk.js"
+  },
+  {
+    "revision": "bec63ccb2fefea030a5b",
+    "url": "/itu/static/js/runtime-main.91f8bdd0.js"
+  }
+]);
